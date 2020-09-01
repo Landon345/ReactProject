5e3c3e3025db87d6c45c79cf5affa34e4322b2f5
Landon Schlangen <landonschlangen@Landons-MacBook-Pro.local>
2020-09-01 12:36:54 -0500

    Updates

---
+++ precache-manifest.e16cf0f73bff1640b45e4539e8cd7b96.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e918fb7bece4d3936aad6feb215542da",
+    "url": "/Deal-or-no-deal-game/index.html"
+  },
+  {
+    "revision": "292701cd6eb481a37e9b",
+    "url": "/Deal-or-no-deal-game/static/css/main.f05016c4.chunk.css"
+  },
+  {
+    "revision": "3211c5ca9ced2bea9a63",
+    "url": "/Deal-or-no-deal-game/static/js/2.8914315c.chunk.js"
+  },
+  {
+    "revision": "292701cd6eb481a37e9b",
+    "url": "/Deal-or-no-deal-game/static/js/main.b5395099.chunk.js"
+  },
+  {
+    "revision": "bbf12eabd0c60d90b5cf",
+    "url": "/Deal-or-no-deal-game/static/js/runtime-main.29f64f81.js"
+  }
+]);
